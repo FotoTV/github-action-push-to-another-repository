19573d010d5cf3385e7055b84521240ae1e475b1
Gerhard Killesreiter <gerhard@killesreiter.de>
2020-09-01 12:27:19 +0200

    Revert "crank up verboseness for debugging"
    This reverts commit 9ea96358d78c626b56e4d98554bcb380e3620785.

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ ls -la "$CLONE_DIR"
 echo "running composer install"
 ls -la
 ls -la drupal/sites/all/modules/custom
-cd "$COMPOSER_DIRECTORY" && composer install -vvv && cd -
+cd "$COMPOSER_DIRECTORY" && composer install && cd -
 ls -la drupal/sites/all/modules/custom
 
 # rm .git repos in subdirectories
